fc363015c8aac92a842ab39e0eb28ab0b502ffb1
Steve Vaneeckhout <6131869+SteveVaneeckhout@users.noreply.github.com>
2026-05-01 15:33:39 +0200

    Add test coverage settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,19 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "node",
     include: ["test/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "lcov"],
+      include: ["src/**"],
+      exclude: ["src/index.ts"],
+      thresholds: {
+        lines: 100,
+        branches: 100,
+        functions: 100,
+        statements: 100,
+      },
+    },
   },
 });
